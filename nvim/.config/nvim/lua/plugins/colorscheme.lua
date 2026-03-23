670cad33709b8d765ed556d0cb849d464fccd413
Lauri Makkonen <ag4039@student.jamk.fi>
2026-03-23 20:00:23 +0200

    updated nvim configs

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -11,8 +11,16 @@ return {
     end,
   },
 
-  -- VS Code theme
+  -- Gruvbox material theme
   {
-    "Mofiqul/vscode.nvim",
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
   },
 }
