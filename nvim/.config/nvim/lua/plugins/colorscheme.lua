83d1340708728852c8306df9b7afdde0f180636e
Lauri Makkonen <ag4039@student.jamk.fi>
2026-03-23 20:20:43 +0200

    updated nvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -10,17 +10,4 @@ return {
       vim.cmd.colorscheme("arctic")
     end,
   },
-
-  -- Gruvbox material theme
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_enable_italic = true
-      vim.cmd.colorscheme("gruvbox-material")
-    end,
-  },
 }
