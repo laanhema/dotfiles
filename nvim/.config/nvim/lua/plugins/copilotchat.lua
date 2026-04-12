f94875212b87f15987fe2ff80db4ed27ff05ca36
Lauri Makkonen <ag4039@student.jamk.fi>
2026-04-13 01:33:26 +0300

    test commit
    test description

---
+++ nvim/.config/nvim/lua/plugins/copilotchat.lua
@@ -0,0 +1,12 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  opts = {
+    model = "gpt-4.1",
+    temperature = 0.1,
+    window = {
+      layout = "vertical",
+      width = 0.5,
+    },
+    auto_insert_mode = true,
+  },
+}
