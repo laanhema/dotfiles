03b743f335221a40a0412cc46c9a9f502d4eb92f
Lauri Makkonen <ag4039@student.jamk.fi>
2026-04-13 03:08:41 +0300

    update nvim files

--- nvim/.config/nvim/lua/plugins/copilotchat.lua
+++ nvim/.config/nvim/lua/plugins/copilotchat.lua
@@ -1,13 +1,17 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "nvim-telescope/telescope.nvim" },
+    { "nvim-lua/plenary.nvim" },
+  },
   opts = {
     model = "gemini-3.1-pro-preview",
     temperature = 0.1,
+    auto_insert_mode = true,
+    auto_follow_cursor = true, -- Automatically scroll with output
     window = {
       layout = "vertical",
       width = 0.35,
-      title = "Copilot Chat",
     },
-    auto_insert_mode = true,
   },
 }
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,18 @@ return {
         show_hidden = true,
         ignored = true,
       },
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
     })
   end,
 }
