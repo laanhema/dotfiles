8f9e2bde2cfb6897568cda01ef2b357285f7f23e
Lauri Makkonen <ag4039@student.jamk.fi>
2026-04-13 04:28:47 +0300

    show hidden files in snacks explorer by default, word wrap on by default

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,22 @@
 return {
-  -- Arctic theme
+  -- 1. Install the theme plugin (without the config function)
   {
     "rockyzhang24/arctic.nvim",
     branch = "v2",
     dependencies = { "rktjmp/lush.nvim" },
     priority = 1000,
     lazy = false,
-    config = function()
-      vim.cmd.colorscheme("arctic")
-    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+  },
+  -- 2. Tell LazyVim to use it as the default
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "arctic",
+    },
   },
 }
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,23 +1,21 @@
 return {
   "folke/snacks.nvim",
-  config = function()
-    require("snacks").setup({
-      explorer = {
-        show_hidden = true,
-        ignored = true,
-      },
-      picker = {
-        sources = {
-          explorer = {
-            hidden = true,
-            ignored = true,
-          },
-          files = {
-            hidden = true,
-            ignored = true,
-          },
+  opts = {
+    explorer = {
+      show_hidden = true,
+      ignored = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+        files = {
+          hidden = true,
+          ignored = true,
         },
       },
-    })
-  end,
+    },
+  },
 }
